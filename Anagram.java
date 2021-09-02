1372b7b8d48267696b59230b4500f04b4bb8ac58
Linus <linushvenfelt@gmail.com>
2021-09-02 13:11:48 +0200

    anagrams done

--- Anagram.java
+++ Anagram.java
@@ -1,9 +1,34 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Anagram {
     public static void main(String[] args) {
         Scanner Scan = new Scanner(System.in);
         System.out.println("Enter a word to find the anagrams.");
-        String input = Scan.next(); // Wait for input
+        String input = Scan.nextLine(); // Wait for input
+
+        try (Stream<String> file = Files.lines(Paths.get("wordlist"))) {
+            file.forEach(line -> {
+                if (input.length() == line.length()) { // If the words are the same length
+
+                    char[] inputChars = input.toCharArray();
+                    char[] lineChars = line.toCharArray();
+
+                    Arrays.sort(inputChars);
+                    Arrays.sort(lineChars);
+
+                    if (Arrays.equals(inputChars, lineChars)) { // If the char ararys are equal
+                        System.out.println(line);
+                    }
+                }
+            });
+        } catch (IOException error) {
+            System.out.println(error);
+        }
+
     }
 }
--- SquareRoot.java
+++ SquareRoot.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class SquareRoot {
     public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+
+        System.out.println("Enter an integer to estimate the square root:");
+        var input = Scan.nextInt(); // Wait for input
+
+
+        int r = input / 2;
+
+        while (input - r > 0.001) {
+            r = input / input;
+            input = (input + r) / 2;
+            System.out.print(input);
+        }
+
+        System.out.print(input);
         
     }
 }
